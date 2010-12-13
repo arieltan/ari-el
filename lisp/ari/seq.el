e4834d80a09e9b0052c05df870cc7f99eeca523f
fukamachi <e.arrows@gmail.com>
2010-12-13 23:44:57 +0900

    added functions, group, n.., 1.. and 0.., to ari/seq.el

--- lisp/ari/seq.el
+++ lisp/ari/seq.el
@@ -29,5 +29,27 @@
                        (rec (cdr x) acc))))))
     (rec x nil)))
 
+(defun ari-seq:group (source n)
+  (if (not (listp source)) (error "group: not list"))
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons
+                              (subseq source 0 n)
+                              acc))
+                   (nreverse
+                    (cons source acc))))))
+    (if source (rec source nil) nil)))
+
+(defun ari-seq:n.. (start end)
+  (loop for i from start upto end collect i))
+
+(defun ari-seq:1.. (end)
+  (ari-seq:n.. 1 end))
+
+(defun ari-seq:0.. (end)
+  (ari-seq:n.. 0 end))
+
 (provide 'ari/seq)
 ;; ari/seq.el ends here
--- test/ari/seq.el
+++ test/ari/seq.el
@@ -22,4 +22,24 @@
   (expect '(r i l "e" "m" "a" "c" "s")
     (let ((to-list '("e" "m" "a" "c" "s")))
       (ari-seq:append-to-list to-list '(a r i e l) nil #'string=)
-      to-list)))
+      to-list))
+
+  (desc "ari-seq:flatten")
+  (expect '(1 2 3 4)
+    (ari-seq:flatten '(1 (2 3) 4)))
+
+  (desc "ari-seq:group")
+  (expect '((e m) (a c) (s))
+    (ari-seq:group '(e m a c s) 2))
+
+  (desc "ari-seq:n..")
+  (expect '(3 4 5 6 7)
+    (ari-seq:n.. 3 7))
+
+  (desc "ari-seq:1..")
+  (expect '(1 2 3 4 5)
+    (ari-seq:1.. 5))
+
+  (desc "ari-seq:0..")
+  (expect '(0 1 2 3 4)
+    (ari-seq:0.. 4)))
