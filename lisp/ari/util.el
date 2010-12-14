c58911e3e7905bad53e0e5d35af9ab95537aa6f8
fukamachi <e.arrows@gmail.com>
2010-12-14 13:05:42 +0900

    added ari-util:compose to compose functions into a function.

--- lisp/ari/util.el
+++ lisp/ari/util.el
@@ -1,3 +1,19 @@
-;;; sample.
+;; ari/util.el - Common utilities.
+
+;; This file is a part of the ari.el (http://github.com/arielnetworks/ari-el)
+;; Copyright (c) 2010 Ariel Networks, Inc.
+;; For the full copyright and license information, please see the COPYRIGHT
+
+;;; Code:
+
+(require 'cl)
+
+(defun ari-util:compose (&rest functions)
+  "Compose FUNCTIONS right-associatively, returning a function"
+  #'(lambda (x)
+      (reduce #'funcall functions
+              :initial-value x
+              :from-end t)))
 
 (provide 'ari/util)
+;; ari/util.el ends here
