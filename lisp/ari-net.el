e9e95c0758bb40b3b500f2dfddf0b6800566c756
fukamachi <e.arrows@gmail.com>
2010-12-20 13:30:23 +0900

    fixed `ari-net:machine-ip-address' to not to use `aif'.

--- lisp/ari-net.el
+++ lisp/ari-net.el
@@ -8,8 +8,9 @@
 
 (defun ari-net:machine-ip-address (dev)
   "Return IP address of a network device."
-  (aif (network-interface-info dev)
-       (format-network-address (car it) t)))
+  (let ((info (network-interface-info dev)))
+    (and info
+         (format-network-address (car info) t))))
 
 (provide 'ari-net)
 ;; ari-net.el ends here
